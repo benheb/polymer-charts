4c5d16f1bc649dc5ee9d9424b4e6d45103908136
brendan <brendan.heberton@gmail.com>
2014-09-29 12:29:24 -0600

    deploy grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,6 +60,12 @@ module.exports = function (grunt) {
           '<%= yeoman.app %>/elements/{,*/}*.{scss,sass}'
         ],
         tasks: ['sass:server', 'autoprefixer:server']
+      },
+      'gh-pages': {
+        options: {
+          base: 'dist'
+        },
+        src: ['**']
       }
     },
     // Compiles Sass to CSS and generates necessary files if requested
